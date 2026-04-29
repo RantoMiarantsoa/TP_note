7c4f95c5cb4c9f9027cf5d502126de0a30124e01
Arinaivo Andriatsihafa Ranto Miarantsoa <andriatsihafamiarantsoa@gmail.com>
2026-04-29 10:19:16 +0300

    Back
    Baks

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,7 +6,12 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('/produits', 'Produit::index');
-$routes->get('/produits/(:num)', 'Produit::show/$1');
+$routes->get('/note', 'Note::index');
+$routes->post('/note/create', 'Note::create');
+$routes->post('/note/(:num)', 'Note::findByEtudiant/$1');
+$routes->get('/etudiant', 'Etudiant::index');
+$routes->get('/etudiant/(:num)', 'Etudiant::show/$1');
+$routes->get('/etudiant/create', 'Etudiant::create');
+
 
 
--- app/Controllers/Etudiant.php
+++ app/Controllers/Etudiant.php
@@ -1,8 +1,20 @@
 <?php
 namespace App\Controllers;
+use App\Models\EtudiantModel;
 
 class Etudiant extends BaseController{
     public function index(){
-        
+        $etudiantModel ⁼ new EtudiantModel();
+        $liste_etudiant = etudiantModel->getAll();
+
+        if(empty($liste_etudiant)){
+             return redirect()->to('/')
+                ->withInput()
+                ->with('Liste non valide');
+        }
+        $data[
+            'nom',
+            ''
+        ]
     }
 }
